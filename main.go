52a94ad4001d8e830a6f85d4e8a5f2dc4dd88570
Marc-Andrea Tarnutzer <mail@marctarnutzer.com>
2018-04-04 22:23:03 +0200

    implemented person correlation function

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 	"io"
 	"log"
 	"strconv"
+	"math"
 )
 
 type User struct {
 	UserID uint32
 	Ratings []float32
+	simToUsers []float32
 }
 
 func loadCSV() ([]User, []string) {
@@ -49,8 +51,46 @@ func loadCSV() ([]User, []string) {
 	return users, movies
 }
 
+// Pearson correlation to compute similarity between users
+func pearsonCorrelation(users []User) []User {
+	for i := 0; i < len(users); i++ {
+		var avg_A, avg_B float32
+		var notNull uint32
+		for j := 0; j < len(users[i].Ratings); j++ {
+			avg_A += users[i].Ratings[j]
+			if users[i].Ratings[j] != 0 {
+				notNull += 1
+			}
+		}
+		avg_A = avg_A / float32(notNull)
+		for j := 0; j < len(users); j++ {
+			notNull = 0
+			for k := 0; k < len(users[j].Ratings); k++ {
+				avg_B += users[j].Ratings[k]
+				if users[j].Ratings[k] != 0 {
+					notNull += 1
+				}
+			}
+			avg_B = avg_B / float32(notNull)
+			var p1, p2, p3 float32
+			for k := 0; k < len(users[j].Ratings); k++ {
+				if users[j].Ratings[k] != 0 && users[i].Ratings[k] != 0 {
+					p1 += (users[i].Ratings[k] - avg_A) * (users[j].Ratings[k] - avg_B)
+					p2 += (users[i].Ratings[k] - avg_A) * (users[i].Ratings[k] - avg_A)
+					p3 += (users[j].Ratings[k] - avg_B) * (users[j].Ratings[k] - avg_B)
+				}
+			}
+			users[i].simToUsers = append(users[i].simToUsers, float32((float64(p1) / (math.Sqrt(float64(p2)) * math.Sqrt(float64(p3))))))
+		}
+	}
+	return users
+}
+
 func main() {
 	//users, movies := loadCSV()
 	//fmt.Println("Done! %v %v", users, movies)
+	users, _ := loadCSV()
+	users = pearsonCorrelation(users)
+	fmt.Println("Similarity values of user 0: ", users[0].simToUsers)
 	fmt.Println("Done!")
 }
